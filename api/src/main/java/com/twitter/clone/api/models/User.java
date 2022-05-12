f7914df3d360f4f833aa758693e669e94045e971
Victor Zevallos <victorze@protonmail.com>
2022-05-11 19:19:26 -0500

    Convert indentation to spaces

--- api/src/main/java/com/twitter/clone/api/models/User.java
+++ api/src/main/java/com/twitter/clone/api/models/User.java
@@ -15,15 +15,15 @@ import lombok.Data;
 @Data
 public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private String username;
-	private String name;
-	private String bio;
-	private String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String name;
+    private String bio;
+    private String password;
 
-	@CreatedDate
-	private Instant createdAt;
+    @CreatedDate
+    private Instant createdAt;
 
 }
